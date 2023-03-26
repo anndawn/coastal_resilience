849f993733ad23945dd9fad214bf39c3a748b8a2
anndawn <35586461+anndawn@users.noreply.github.com>
2023-03-26 16:09:49 +0200

    update pictures

--- scripts.js
+++ scripts.js
@@ -1,5 +1,8 @@
 const texts = [
-    'The highlighted river systems are flagged as pollution hot spots due to pollution from sewer overflows or/ damaged or washed away sewer lines. Grey dots are waste water treatment works along the rivers.',
+    'The highlighted river systems are flagged as pollution hot spots due to pollution from sewer overflows or/ damaged or washed away sewer lines.',
+    'Quarry Road is on the Palmiet river, one of which is flagged as a pollution hot spot because of sewer overflows.',
+    'Talbot tested the river at Papwa Sewgolum on Palmiet River next to Quarry Road. The E.coli level started raising after the flood and remains high today.',
+    'Grey dots are all the wastewater treatment works along the polluted rivers.',
     'Around end of April and early May, at least 10 water treatment works registered high level failure on E.coli levels, having over 80000 E.coli per 100ml of water.',
     'By early May, Durban beaches registered high E.coli levels, all red triangles are beach testing point showing poor conditions.',
     'Testing at Umhlanga Rocks Main beach shows over 80000 E.coli per 100ml of water, which is 160 times more than the acceptable level of 500 E.coli per 100ml of water.',
@@ -7,11 +10,14 @@ const texts = [
   ];
   
   const imagePaths = [
-    './img/wwtw_river.png',
-    './img/high_failures.png',
-    './img/beach.png',
-    './img/main_beach.png',
-    './img/dec_map.png'
+    './img/map1.jpg',
+    './img/map2.jpg',
+    './img/map8.jpg',
+    './img/map3.jpg',
+    './img/map4.jpg',
+    './img/map5.jpg',
+    './img/map6.jpg',
+    './img/map7.jpg',
   ];
   
   const textOverlay = document.getElementById('text-overlay');
