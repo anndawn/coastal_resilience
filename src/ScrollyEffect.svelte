0fe968d6c79f6a4dbf01657ed827df453950c2a8
anndawn <35586461+anndawn@users.noreply.github.com>
2023-03-27 17:19:38 +0200

    update image and text

--- src/ScrollyEffect.svelte
+++ src/ScrollyEffect.svelte
@@ -1,25 +1,25 @@
 <script>
     let texts = [
-    'The highlighted river systems are flagged as pollution hot spots due to pollution from sewer overflows or/ damaged or washed away sewer lines.',
-    'Quarry Road is on the Palmiet river, one of which is flagged as a pollution hot spot because of sewer overflows.',
-    'Talbot tested the river at Papwa Sewgolum on Palmiet River next to Quarry Road. The E.coli level started raising after the flood and remains high today.',
-    'Grey dots are all the wastewater treatment works along the polluted rivers.',
-    'Around end of April and early May, at least 10 water treatment works registered high level failure on E.coli levels, having over 80000 E.coli per 100ml of water.',
-    'By early May, Durban beaches registered high E.coli levels, all red triangles are beach testing point showing poor conditions.',
-    'Testing at Umhlanga Rocks Main beach shows over 80000 E.coli per 100ml of water, which is 160 times more than the acceptable level of 500 E.coli per 100ml of water.',
-    'By end of 2022, 7 of the 10 water treatment still has poor microbiological compliance. Testing at the beach still shows critical E.coli levels./ All beaches had high e.coli levels in December'
+    'Late April last year, the municipality said highlighted river systems are flagged as pollution hot spots due to pollution from sewer overflows or/ damaged or washed away sewer lines.',
+    'Palmiet river is one of these flagged pollution hot spots. Quarry Road is situated alongside this river. Palmiet river is a tributary of uMgeni River. Four wastewater treatment plants discharge into uMgeni river or tributary of uMgeni River.',
+    'Talbot carried out a water sample test at Papwa Sewgolum on the Palmiet River, near Quarry Road. After the April flood, the E.coli concentration has significantly increased and remains high to this day. On February 21, 2023, the E.coli level at Papwa Sewgolum reached 457,000 counts per 100ml, indicating a critical water situation, as levels above 400 are considered critical.',
+    'Water treatment plants are situated along the banks of the rivers. The grey dots denote the locations of all the wastewater treatment facilities present along the rivers.',
+    'According to data from the Department of Water and Sanitation’s IRIS dashboard, in late April and early May, at least 10 water treatment facilities reported high E.coli levels, with concentrations reaching 80,000 E.coli per 100ml of water, indicating high-level failures.',
+    'Based on municipal data from early May, Durban beaches registered high E.coli levels. All red triangles on the map represent beach testing points, indicating poor water quality conditions.',
+    'On May 5, 2022, water testing at Umhlanga Rocks Main beach has revealed an alarming concentration of E.coli : 80,000 per 100ml of water. This is 160 times higher than the acceptable range of 250-500 E.coli per 100ml.',
+    "The most recent three months data accessed on 21 Feb 2023 from the Department of Water and Sanitation’s IRIS dashboard showed 7 of the 10 water treatment still has bad microbiological compliance. As per the municipality's records, water testing at the beach around 20th December 2022 shows critical E.coli levels. Umlaas beach shows over 24196 E.coli per 100ml of water."
   ];
   
   
     let imagePaths = [
       '../img/map1.jpg',
       '../img/map2.jpg',
-      '../img/map8.jpg',
       '../img/map3.jpg',
       '../img/map4.jpg',
-      '../img/map6.jpg',
       '../img/map5.jpg',
+      '../img/map6.jpg',
       '../img/map7.jpg',
+      '../img/map8.jpg',
     ];
   
     let textOverlay;
@@ -54,10 +54,12 @@
   position: fixed;
   top: 0;
   left: 0;
-  width: 100%;
+ 
+  width: 80%;
   height: 100%;
   background-image: url('./img/map1.png');
-  background-size: cover;
+  background-size: contain;
+  background-repeat: no-repeat;
   background-position: center;
   z-index: -2; /* Change this value to -2 */
   transition: background-image 1s ease-in-out;
@@ -73,7 +75,8 @@
 
 .text-overlay {
     position: fixed;
-    font-size: 2rem;
+    
+    font-size: 1.5rem;
     color: #fff;
     text-align: left;
     padding: 1rem;
@@ -81,7 +84,7 @@
     left: 0;
     top: 30%;
     transform: translateY(-50%);
-    max-width: 600px;
+    max-width: 650px;
 }
 
 /* .scroll-item {
@@ -98,7 +101,7 @@
 }
 
 .text-overlay {
-  width: 600px;
+  width: 650px;
 }
 .chart{
     background-color: #F5F5F5;
